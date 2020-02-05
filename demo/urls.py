b7d24a11559d8513ed447e656e0e6d6d6ff57f56
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-02-05 10:58:26 +0530

    Initial Commit - Aniket Aryamane

---
+++ demo/urls.py
@@ -0,0 +1,43 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.views.generic import TemplateView, RedirectView
+
+from rest_framework_swagger.views import get_swagger_view
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="home.html"), name='home'),
+    path('signup/', TemplateView.as_view(template_name="signup.html"), name='signup'),
+    path('email-verification/', TemplateView.as_view(
+        template_name="email_verification.html"), name='email-verification'),
+    path('login/', TemplateView.as_view(template_name="login.html"),
+         name='login'),
+    path('logout/', TemplateView.as_view(template_name="logout.html"),
+         name='logout'),
+    path('password-reset/',
+         TemplateView.as_view(template_name="password_reset.html"),
+         name='password-reset'),
+    path('password-reset/confirm/',
+         TemplateView.as_view(template_name="password_reset_confirm.html"),
+         name='password-reset-confirm'),
+
+    path('user-details/',
+         TemplateView.as_view(template_name="user_details.html"),
+         name='user-details'),
+    path('password-change/',
+         TemplateView.as_view(template_name="password_change.html"),
+         name='password-change'),
+
+
+    # this url is used to generate email content
+    path('password-reset/confirm/<slug:uidb64>/<slug:token>/',
+         TemplateView.as_view(template_name="password_reset_confirm.html"),
+         name='password_reset_confirm'),
+
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('account/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/profile/', RedirectView.as_view(url='/',
+                                                   permanent=True), name='profile-redirect'),
+    path('docs/', get_swagger_view(title='API Docs'), name='api_docs')
+]
